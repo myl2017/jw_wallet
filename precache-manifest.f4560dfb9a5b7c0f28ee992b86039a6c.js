625eeab026e69300e97864061f367c6968c41053
mengyl <706695013@qq.com>
2020-03-23 19:13:18 +0800

    deploy

---
+++ precache-manifest.f4560dfb9a5b7c0f28ee992b86039a6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740ca67c7f92b786efc8",
+    "url": "/jw_wallet/css/app.cf117d6b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/jw_wallet/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c99e61cf8c4adf2f5a045c9b5918bcd7",
+    "url": "/jw_wallet/index.html"
+  },
+  {
+    "revision": "d6f12e823af2eff61309",
+    "url": "/jw_wallet/js/about.7eb3a9ff.js"
+  },
+  {
+    "revision": "740ca67c7f92b786efc8",
+    "url": "/jw_wallet/js/app.6fa06ac6.js"
+  },
+  {
+    "revision": "fbf639e1deb7df77a467",
+    "url": "/jw_wallet/js/chunk-vendors.83bea35d.js"
+  },
+  {
+    "revision": "6ad77bb65f8f438ba6dee3f013b7983d",
+    "url": "/jw_wallet/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jw_wallet/robots.txt"
+  }
+]);
